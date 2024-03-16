12086589e6e743a6363d44694a139e757bdb28be
mikhail_pr <m.prus@a1.by>
2024-03-16 14:57:14 +0300

    Initialization

---
+++ PfBitrixConverter/src/main/java/eqt/PfBitrixConverter/PfBitrixConverterApplication.java
@@ -0,0 +1,13 @@
+package eqt.PfBitrixConverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfBitrixConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfBitrixConverterApplication.class, args);
+	}
+
+}
